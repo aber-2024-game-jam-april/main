dc00c5b13c007af1eeef7650a6f4839a8340e887
brunskins <62477802+brunskins@users.noreply.github.com>
2024-04-21 10:43:10 +0100

    adding art assets

---
+++ art-assets/new pixel art.tsx
@@ -0,0 +1,154 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="new pixel art" tilewidth="12" tileheight="12" tilecount="50" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="12" height="12" source="grass block.png"/>
+ </tile>
+ <tile id="3">
+  <image width="12" height="12" source="grass top mid.png"/>
+ </tile>
+ <tile id="4">
+  <image width="12" height="12" source="left ledge.png"/>
+ </tile>
+ <tile id="5">
+  <image width="12" height="12" source="right ledge.png"/>
+ </tile>
+ <tile id="6">
+  <image width="12" height="12" source="grass hill right.png"/>
+ </tile>
+ <tile id="7">
+  <image width="12" height="12" source="grass hill left.png"/>
+ </tile>
+ <tile id="8">
+  <image width="12" height="12" source="grass hill corner left.png"/>
+ </tile>
+ <tile id="9">
+  <image width="12" height="12" source="grass hill corner right.png"/>
+ </tile>
+ <tile id="10">
+  <image width="12" height="12" source="cave/cave hill corner left.png"/>
+ </tile>
+ <tile id="11">
+  <image width="12" height="12" source="cave/cave hill corner right.png"/>
+ </tile>
+ <tile id="12">
+  <image width="12" height="12" source="cave/cave left hill.png"/>
+ </tile>
+ <tile id="13">
+  <image width="12" height="12" source="cave/cave right hill.png"/>
+ </tile>
+ <tile id="14">
+  <image width="12" height="12" source="cave/cave top.png"/>
+ </tile>
+ <tile id="15">
+  <image width="12" height="12" source="cave/cave undergrownd.png"/>
+ </tile>
+ <tile id="16">
+  <image width="12" height="12" source="cave/cave right ledge.png"/>
+ </tile>
+ <tile id="17">
+  <image width="12" height="12" source="cave/cave left ledge.png"/>
+ </tile>
+ <tile id="24">
+  <image width="12" height="12" source="cave/cave side central left.png"/>
+ </tile>
+ <tile id="25">
+  <image width="12" height="12" source="cave/cave side central right.png"/>
+ </tile>
+ <tile id="26">
+  <image width="12" height="12" source="transition/grass to cave.png"/>
+ </tile>
+ <tile id="27">
+  <image width="12" height="12" source="transition/grass to cave side left.png"/>
+ </tile>
+ <tile id="28">
+  <image width="12" height="12" source="transition/grass to cave side right.png"/>
+ </tile>
+ <tile id="29">
+  <image width="12" height="12" source="cave/cave side top left.png"/>
+ </tile>
+ <tile id="30">
+  <image width="12" height="12" source="cave/cave side top right.png"/>
+ </tile>
+ <tile id="32">
+  <image width="12" height="12" source="msic objects/spike botom.png"/>
+ </tile>
+ <tile id="33">
+  <image width="12" height="12" source="msic objects/spike down.png"/>
+ </tile>
+ <tile id="34">
+  <image width="12" height="12" source="msic objects/spike left.png"/>
+ </tile>
+ <tile id="35">
+  <image width="12" height="12" source="msic objects/spike right.png"/>
+ </tile>
+ <tile id="37">
+  <image width="12" height="12" source="bricks/bricks bad.png"/>
+ </tile>
+ <tile id="38">
+  <image width="12" height="12" source="bricks/bricks good.png"/>
+ </tile>
+ <tile id="39">
+  <image width="12" height="12" source="bricks/bricks ruined.png"/>
+ </tile>
+ <tile id="40">
+  <image width="12" height="12" source="msic objects/cabnet botom.png"/>
+ </tile>
+ <tile id="41">
+  <image width="12" height="12" source="msic objects/cabnet top.png"/>
+ </tile>
+ <tile id="42">
+  <image width="12" height="12" source="msic objects/table end left.png"/>
+ </tile>
+ <tile id="43">
+  <image width="12" height="12" source="msic objects/table end right.png"/>
+ </tile>
+ <tile id="44">
+  <image width="12" height="12" source="msic objects/table middle.png"/>
+ </tile>
+ <tile id="46">
+  <image width="12" height="12" source="msic objects/chair left.png"/>
+ </tile>
+ <tile id="47">
+  <image width="12" height="12" source="msic objects/chair right.png"/>
+ </tile>
+ <tile id="48">
+  <image width="12" height="12" source="msic objects/book.png"/>
+ </tile>
+ <tile id="50">
+  <image width="12" height="12" source="msic objects/rad barrel top.png"/>
+ </tile>
+ <tile id="51">
+  <image width="12" height="12" source="msic objects/rad barrel botom.png"/>
+ </tile>
+ <tile id="52">
+  <image width="12" height="12" source="transition/bricks bad to black.png"/>
+ </tile>
+ <tile id="53">
+  <image width="12" height="12" source="transition/bricks good to black.png"/>
+ </tile>
+ <tile id="54">
+  <image width="12" height="12" source="transition/bricks ruined to black.png"/>
+ </tile>
+ <tile id="55">
+  <image width="12" height="12" source="transition/brick to cave left.png"/>
+ </tile>
+ <tile id="56">
+  <image width="12" height="12" source="transition/brick to cave right.png"/>
+ </tile>
+ <tile id="57">
+  <image width="12" height="12" source="wood bridge/bridge center.png"/>
+ </tile>
+ <tile id="58">
+  <image width="12" height="12" source="wood bridge/bridge end left.png"/>
+ </tile>
+ <tile id="59">
+  <image width="12" height="12" source="wood bridge/bridge end right.png"/>
+ </tile>
+ <tile id="60">
+  <image width="12" height="12" source="wood bridge/bridge leg left.png"/>
+ </tile>
+ <tile id="61">
+  <image width="12" height="12" source="wood bridge/bridge leg right.png"/>
+ </tile>
+</tileset>
