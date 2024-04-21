00942a529522343b7b16422b77d11409f67eac84
dac125 <dac125@aber.ac.uk>
2024-04-21 12:44:09 +0100

    Adding settings class

---
+++ Settings.py
@@ -0,0 +1,15 @@
+
+class Setting():
+
+    SETTING = Setting()
+
+
+    def __init__():
+        print("Settings.init() was run")
+
+
+    @staticmethod
+    def getSetting(self):
+        return self.SETTING
+
+    
